146aef9101899bdf7af92a2b04e18eb0dd96420f
Aufar S <110440438+AufarSS@users.noreply.github.com>
2022-12-17 17:19:39 +0700

    Update Performance.py

--- 7. Performance/Performance.py
+++ 7. Performance/Performance.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 
-class testday17(unittest.TestCase):
+class test_Performance_Aufar(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
